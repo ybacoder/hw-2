07a8702df23a82d3dbc3a11e5b0ed83ae20c7b66
ybacoder <ybacoder@gmail.com>
2019-11-16 13:19:57 -0600

    Analyzed the data to calculate the required parameters

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,11 +17,11 @@ In addition, your final script should both print the analysis to the terminal an
 """
 
 import os
+
 file_path = os.path.join("Resources", "budget_data.csv")
 
 date = []
 profit = []
-budget = {}
 with open(file_path, 'r') as file_in:
     file_in.readline()
     for line in file_in:
@@ -29,5 +29,23 @@ with open(file_path, 'r') as file_in:
         date.append(line[0])
         profit.append(int(line[1]))
 
-print(date)
-print(profit)
+total_months = len(date)
+print(total_months)
+
+total_profit = sum(profit)
+print(total_profit)
+
+profit_df_date = date[1:]
+profit_df = [profit_current - profit_prev for profit_current, profit_prev in zip(profit[1:], profit[:-1])]
+average_profit_df = sum(profit_df) / len(profit_df)
+print(average_profit_df)
+
+max_profit_df = max(profit_df)
+max_profit_df_index = profit_df.index(max_profit_df)
+max_profit_date = date[max_profit_df_index + 1]
+print(max_profit_date, max_profit_df)
+
+min_profit_df = min(profit_df)
+min_profit_df_index = profit_df.index(min_profit_df)
+min_profit_date = date[min_profit_df_index + 1]
+print(min_profit_date, min_profit_df)
