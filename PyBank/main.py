3885b939882a0c78a07b384a0225c3509a4d4cab
ybacoder <ybacoder@gmail.com>
2019-11-19 21:14:07 -0600

    Streamlined code and cleaned up the formatting

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,29 +23,30 @@ file_path = os.path.join("Resources", "budget_data.csv")
 date = []
 profit = []
 with open(file_path, 'r') as file_in:
-    file_in.readline()
+    header = file_in.readline()
     for line in file_in:
         line = line.split(',')
         date.append(line[0])
         profit.append(int(line[1]))
 
 total_months = len(date)
-print(total_months)
 
 total_profit = sum(profit)
-print(total_profit)
 
 profit_df_date = date[1:]
 profit_df = [profit_current - profit_prev for profit_current, profit_prev in zip(profit[1:], profit[:-1])]
 average_profit_df = sum(profit_df) / len(profit_df)
-print(average_profit_df)
 
 max_profit_df = max(profit_df)
 max_profit_df_index = profit_df.index(max_profit_df)
 max_profit_date = date[max_profit_df_index + 1]
-print(max_profit_date, max_profit_df)
-
 min_profit_df = min(profit_df)
 min_profit_df_index = profit_df.index(min_profit_df)
 min_profit_date = date[min_profit_df_index + 1]
-print(min_profit_date, min_profit_df)
+
+print("Financial Analysis\n-------------------------------")
+print("Total Months: ", total_months)
+print("Total: ${:,.0f}".format(total_profit))
+print("Average Change: ${:,.2f}".format(average_profit_df))
+print("Greatest Increase in Profits: {} ${:,.0f}".format(max_profit_date, max_profit_df))
+print("Greatest Decrease in Profits: {} ${:,.0f}".format(min_profit_date, min_profit_df))
