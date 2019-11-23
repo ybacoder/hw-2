e06b48af90394fae07d0d1aeca4787cbc8c83158
ybacoder <ybacoder@gmail.com>
2019-11-22 21:31:11 -0600

    Add basic code to main.py to import data file. Add state abbrev .py file

---
+++ PyBoss/main.py
@@ -0,0 +1,41 @@
+"""
+Yacub Bholat
+Data Analysis and Visualization Boot Camp
+Python Homework PyPoll
+Due: 2 December 2019
+
+Your company recently decided to purchase a new HR system, and unfortunately for you, the new system requires employee records be stored completely differently.
+
+Your task is to help bridge the gap by creating a Python script able to convert your employee records to the required format. Your script will need to do the following:
+
+Import the employee_data.csv file, which currently holds employee records like the below:
+-Emp ID,Name,DOB,SSN,State
+-214,Sarah Simpson,1985-12-04,282-01-8166,Florida
+-15,Samantha Lara,1993-09-08,848-80-7526,Colorado
+-411,Stacy Charles,1957-12-20,658-75-8526,Pennsylvania
+
+Then convert and export the data to use the following format instead:
+-Emp ID,First Name,Last Name,DOB,SSN,State
+-214,Sarah,Simpson,12/04/1985,***-**-8166,FL
+-15,Samantha,Lara,09/08/1993,***-**-7526,CO
+-411,Stacy,Charles,12/20/1957,***-**-8526,PA
+
+In summary, the required conversions are as follows:
+-The Name column should be split into separate First Name and Last Name columns.
+-The DOB data should be re-written into MM/DD/YYYY format.
+-The SSN data should be re-written such that the first five numbers are hidden from view.
+-The State data should be re-written as simple two-letter abbreviations.
+
+Special Hint: You may find this link to be helpful—Python Dictionary for State Abbreviations.
+https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5
+"""
+
+import os
+import csv
+from us_state_abbrev import us_state_abbrev
+
+in_file_path = os.path.join("employee_data.csv")
+
+with open(in_file_path, 'r') as in_file:
+    for line in in_file:
+        print(line)
