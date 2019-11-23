6b2e2eee110765e5fc6ebe199db718003e2b6dd2
ybacoder <ybacoder@gmail.com>
2019-11-23 12:43:46 -0600

    Added code to transform employee data and print to new .csv file.

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,7 +1,7 @@
 """
 Yacub Bholat
 Data Analysis and Visualization Boot Camp
-Python Homework PyPoll
+Python Homework PyBoss
 Due: 2 December 2019
 
 Your company recently decided to purchase a new HR system, and unfortunately for you, the new system requires employee records be stored completely differently.
@@ -39,19 +39,30 @@ in_file_path = os.path.join("employee_data.csv")
 with open(in_file_path, 'r') as in_file:
     csv_reader = csv.reader(in_file)
     header_in = next(csv_reader)
-    employee_data_in = [line for line in csv_reader]
-print(employee_data_in[-1][1].split(" "))
+    employee_data_in = list(csv_reader)
 
 # create new header list and replace name with first/last name
 header_out = header_in.copy()
 header_out.insert(2, "Last Name")
 header_out.insert(2, "First Name")
 header_out.pop(header_in.index("Name"))
-print(header_in)
-print(header_out)
+
+def date_convert(date):
+    # convert date from yyyy-mm-dd to mm/dd/yyyy
+    date = date.replace("-", "/")
+    date_year = date[:4]
+    date = date[5:] + "/" + date_year
+    return date
+
+def ssn_convert(ssn):
+    # hide first five numbers of ssn
+    return "***-**-" + ssn[-4:]
 
 # create new list with the required parameters
-employee_data_out = [[(employee[0])] + employee[1].split(" ") for employee in employee_data_in]
-for name in employee_data_out:
-    print(name)
+employee_data_out = [[(employee[0])] + employee[1].split(" ") + [date_convert(employee[2])] +\
+     [ssn_convert(employee[3])] + [us_state_abbrev[employee[4]]] for employee in employee_data_in]
 
+with open("employee_data_new.csv", 'w') as out_file:
+    # Output new employee data file to out_file
+    out_file.write(",".join(map(str, header_out)) + "\n")
+    [out_file.write(",".join(map(str, employee)) + "\n") for employee in employee_data_out]
